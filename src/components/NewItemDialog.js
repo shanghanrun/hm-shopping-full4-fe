a3bbe9bae3df6cba5f9431f7dbed63a84395cd1b
shanghanrun <idim7@naver.com>
2024-06-04 16:40:01 +0900

    key=index로 된 것을 key={index+item}의 고유값으로 바꾸었다.

--- src/components/NewItemDialog.js
+++ src/components/NewItemDialog.js
@@ -250,7 +250,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
           </Button>
           <div className="mt-2">
             {stock.map((item, index) => (
-              <Row key={index}>
+              <Row key={`${index}${item}`}>
                 <Col sm={4}>
                   <Form.Select
                     onChange={(event) =>

6973ef2a30f93a9242a9abcc5f9058114b57ba55
shanghanrun <idim7@naver.com>
2024-06-05 14:05:53 +0900

    .

--- src/page/Info.js
+++ src/page/Info.js
@@ -1,7 +1,13 @@
 const Info =()=>{
 
-	return(
+	return(<div>
+		<h5>MoviePage 이용방법</h5>
+		<div>영화사이트로 이동한 후에</div>
+		<img src="/image/movieinfo.png" alt=''/>
+		<div>Choice 페이지를 방문해서 영화를 고르고, Reserve페이지에서 좌석을 여러개 선택합니다.</div>
+		<div>다시 hm-shopping으로 돌아오면 'MyPage'에 예약한 영화와 좌석정보가 나옵니다.</div>
 		<h4>Info Page: Naver Map을 적용하려 준비중...</h4>
+	</div>		
 	)
 }
 

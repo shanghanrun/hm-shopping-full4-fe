7a45e3dc3e0dcd5e1000c05b8f558faed2d82efa
shanghanrun <idim7@naver.com>
2024-06-11 13:12:15 +0900

    .

--- src/page/Info.js
+++ src/page/Info.js
@@ -1,14 +1,18 @@
+import {Container} from 'react-bootstrap'
+
 const Info =()=>{
 
 	return(<div>
-		<h5>MoviePage 이용방법</h5>
-		<div>영화사이트로 이동한 후에</div>
-		<img src="/image/movieinfo.png" alt=''/>
-		<div>Choice 페이지를 방문해서 영화를 고르고, Reserve페이지에서 좌석을 여러개 선택합니다.</div>
-		<div>다시 hm-shopping으로 돌아오면 'MyPage'에 예약한 영화와 좌석정보가 나옵니다.</div>
-		<div>하지만.. 백엔드를 교체하지 못해서.. 영화정보를 당분간 못불러옵니다.</div>
-		<div style={{height:'100px'}}></div>
-		<h4>Info Page: Naver Map을 적용하려 준비중...</h4>
+		<Container>
+			<h5>MoviePage 이용방법</h5>
+			<div>영화사이트로 이동한 후에</div>
+			<img src="/image/movieinfo.png" alt=''/>
+			<div>Choice 페이지를 방문해서 영화를 고르고, Reserve페이지에서 좌석을 여러개 선택합니다.</div>
+			<div>다시 hm-shopping으로 돌아오면 'MyPage'에 예약한 영화와 좌석정보가 나옵니다.</div>
+			<div></div>
+			<div style={{height:'100px'}}></div>
+			<h4>기타 서비스 준비중...</h4>
+		</Container>
 	</div>		
 	)
 }

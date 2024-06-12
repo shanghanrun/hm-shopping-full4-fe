a3505b01446c9017b983e911371683d3062bb95e
shanghanrun <idim7@naver.com>
2024-06-12 18:00:43 +0900

    .

--- src/page/CartPage.js
+++ src/page/CartPage.js
@@ -32,7 +32,7 @@ const CartPage = () => {
               ))
             ) : (
               // (cartCount === 0) ? (
-              (cart.items.length === 0) ? (
+              (cart?.items?.length === 0) ? (
                 <div className="text-align-center empty-bag">
                   <h2>카트가 비어있습니다.</h2>
                   <div>상품을 담아주세요!</div>
